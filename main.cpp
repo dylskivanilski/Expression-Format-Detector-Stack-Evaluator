cbb64c9dcbe5427f0b63edf0cd56a9337020ba73
dylskivanilski <162456407+dylskivanilski@users.noreply.github.com>
2026-03-29 20:29:23 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -39,3 +39,43 @@ int main() {
 
     return 0;
 }
+#ifndef ARRAYSTACK_H
+#define ARRAYSTACK_H
+
+#include <vector>
+#include <stdexcept>
+
+template <typename T>
+class ArrayStack {
+private:
+    std::vector<T> data;
+
+public:
+    void push(const T& value) {
+        data.push_back(value);
+    }
+
+    void pop() {
+        if (data.empty()) {
+            throw std::runtime_error("Stack underflow");
+        }
+        data.pop_back();
+    }
+
+    T top() const {
+        if (data.empty()) {
+            throw std::runtime_error("Stack is empty");
+        }
+        return data.back();
+    }
+
+    bool empty() const {
+        return data.empty();
+    }
+
+    int size() const {
+        return data.size();
+    }
+};
+
+#endif
